7456e83e657e9aa6b92f3249a3068987875f682c
Sergio Burik <sergioburik@gmail.com>
2020-02-05 06:45:18 +0300

    styles fix

--- airtickets/mainapp/views.py
+++ airtickets/mainapp/views.py
@@ -4,7 +4,7 @@ import re
 from django.http import JsonResponse
 from django.shortcuts import render
 from mainapp.models import Depature, Airport, Seat, Aircompany
-import utils
+import parsers
 
 # Create your views here.
 
@@ -17,29 +17,6 @@ def main(request):
 
 
 def search(request):
-    # from_ = request.GET['from']
-    # to = request.GET['to']
-    # date_from = request.GET['date_from']
-    # date_to = request.GET['date_to']
-    #
-    # from_ = Airport.objects.get(name=from_)
-    # to = Airport.objects.get(name=to)
-    #
-    # utils.parse_departures(from_.code, to.code, date_from)
-    # print(datetime.datetime.strptime(date_from, '%Y-%m-%d'))
-    # result = Depature.objects.filter(
-    #                                  flight__departure_point=from_, flight__to=to)
-    #
-    # print(result)
-    #
-    # depatures_seats = []
-    #
-    # for res in result:
-    #     try:
-    #         depatures_seats.append(Seat.objects.get(depature=res.id, is_busy=False))
-    #     except:
-    #         print('Exception')
-
 
     options = Airport.objects.all()
 
@@ -63,7 +40,7 @@ def ajax_departures_scrapper(request):
     from_ = Airport.objects.get(name=from_)
     to = Airport.objects.get(name=to)
 
-    utils.parse_departures(from_.code, to.code, date_from)
+    parsers.parse_departures(from_.code, to.code, date_from)
 
     result = Depature.objects.filter(depature_time__date=datetime.datetime.strptime(date_from, '%Y-%m-%d'),
                                      flight__departure_point=from_,
---
+++ airtickets/parsers.py
@@ -0,0 +1,85 @@
+# coding:utf-8
+import re
+
+import requests
+
+from bs4 import BeautifulSoup
+
+from mainapp.models import Flight, Airport, Aircompany, Depature
+
+def parse_departures(from_, to, date_from):
+    print(from_, to, date_from)
+
+    page = requests.get(f'https://www.aerobilet.net/flight-search/{from_}-{to}/{date_from}/1-0-0/ALL',
+                        )
+    print(f'https://www.aerobilet.net/flight-search/{from_}-{to}/{date_from}/1-0-0/ALL')
+
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    table = soup.findAll("table", {"class": "tablesorter"})[0]
+    cards = table.find_all('tr')
+
+    parsed_data = []
+
+    for card in cards:
+        try:
+            time_from = f'{date_from} {card.find("span", {"class": "sr-time-dep"}).getText()}'
+            name = ''  # кажется, там нет номера рейса
+            plane = ''  # там нет названия самолета
+            time_to = re.sub('[()]', '', f'{date_from} {card.find("span", {"class": "sr-time-arr"}).getText()}')
+            aircompany = card.find("span", {"class": "airline-name-text"}).getText()
+
+
+            try:
+                flight = Flight.objects.filter(departure_point=Airport.objects.get(code=from_),
+                                                  to=Airport.objects.get(code=to),
+                                                  aircompany=Aircompany.objects.get(name__contains=aircompany))[0]
+            except:
+                flight = None
+
+            if flight and not len(Depature.objects.filter(flight=flight,
+                depature_time=time_from)):  # если есть рейс, но нет вылета..
+
+                Depature.objects.create(name=name, plane=plane, depature_time=time_from, arrival_time=time_to,
+                                        flight=flight)
+
+            # parsed_data.append({'time_from': time_from.strip(), 'name': name.strip(), 'plane': plane.strip(),
+            #                     'time_to': time_to.strip(), 'aircompany':aircompany.strip(), 'flight_id':flight_id})
+
+            if not flight:  # если нет такого рейса
+                try:
+                    # пробуем создать рейс и вылет
+                    print(Airport.objects.get(code=from_).id)
+                    print(Airport.objects.get(code=to).id)
+                    print(Aircompany.objects.get(name__contains=aircompany).id)
+
+                    Flight.objects.create(departure_point=Airport.objects.get(code=from_),
+                                  to=Airport.objects.get(code=to),
+                                  aircompany=Aircompany.objects.get(name__contains=aircompany))
+
+                    Depature.objects.create(name=name, plane=plane, depature_time=time_from, arrival_time=time_to,
+                                     flight=flight)
+
+                except Exception as err:  # если нет такой авиакомпании, тогда создаем
+                    Aircompany.objects.create(name=aircompany, rating=0)
+
+                    Flight.objects.create(departure_point=Airport.objects.get(code=from_).id,
+                                  to=Airport.objects.get(code=to).id,
+                                  aircompany=Aircompany.objects.get(name__contains=aircompany).id)
+
+                    Depature.objects.create(name=name, plane=plane, depature_time=time_from, arrival_time=time_to,
+                                     flight=flight)
+
+
+        except Exception as err:
+            print('AttributeError', err)
+    print()
+
+
+def create_flight(departure_point, to, aircompany):
+    Flight.objects.create(departure_point=int(departure_point), to=int(to), aircompany=int(aircompany))
+
+
+def create_departure(name, plane, departure_time, arrival_time, flight):
+    Depature.objects.create(name=name, plane=plane, depature_time=departure_time, arrival_time=arrival_time,
+                            flight=flight)
